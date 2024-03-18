ee03296a88bbb538930e2a08d62ec92ba96da679
安東秀斗 <shuto.ando1@gmail.com>
2024-03-18 13:31:04 +0800

    forth

--- javascript_learning/03/main.js
+++ javascript_learning/03/main.js
@@ -1,21 +1,32 @@
 "use strict";
 
 {
-  const scores = [70, 90, 80, 85];
-  let total = 0;
-  scores.forEach((score, index) => {
-    total += score;
-    // console.log(`${index}: ${score}`);
-  });
-  console.log(`total: ${total}`);
-  console.log(`average: ${total / scores.length}`);
+  // const scores = [70, 90, 80, 85];
+  // let total = 0;
+  // scores.forEach((score, index) => {
+  //   total += score;
+  // console.log(`${index}: ${score}`);
+  // });
+  // console.log(`total: ${total}`);
+  // console.log(`average: ${total / scores.length}`);
   //   scores.push(77, 88);
   //   for (let i = 0; i < scores.length; i++) {
   //     console.log(scores[i]);
   //   }
-
   //   scores.push(10);
   //   scores.pop(10);
   //   scores.unshift(10);
   //   scores.shift(10);
+
+  const scores = {
+    math: 80,
+    english: 90,
+  };
+  console.log(scores[`english`]);
+  scores.math = 40;
+  const entries = Object.entries(scores);
+  entries.forEach((entry) => {
+    console.log(`${entry[0]}: ${entry[1]}`);
+  });
+  // console.log(scores.math);
 }
