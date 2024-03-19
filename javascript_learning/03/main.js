7e84a3c2e28b1b1eb8f968e845cb4e3b9272a0d9
安東秀斗 <shuto.ando1@gmail.com>
2024-03-19 10:09:46 +0800

    fifth

--- javascript_learning/03/main.js
+++ javascript_learning/03/main.js
@@ -1,7 +1,9 @@
 "use strict";
 
 {
-  // const scores = [70, 90, 80, 85];
+  const moreScores = [77, 88];
+  //スプレッド構文
+  const scores = [70, 90, 80, 85, ...moreScores];
   // let total = 0;
   // scores.forEach((score, index) => {
   //   total += score;
@@ -17,16 +19,32 @@
   //   scores.pop(10);
   //   scores.unshift(10);
   //   scores.shift(10);
+  // const scores = {
+  //   math: 80,
+  //   english: 90,
+  // };
+  // console.log(scores[`english`]);
+  // scores.math = 40;
+  // const entries = Object.entries(scores);
+  // entries.forEach((entry) => {
+  //   console.log(`${entry[0]}: ${entry[1]}`);
+  // });
+  // const prices = [100, 150, 200];
+  // const pricesOver150 = prices.filter((price) => {
+  //   return price >= 150;
+  // });
+  // console.log(pricesOver150);
+  //分割代入
+  // const [first, second, third, fourth] = scores;
+  // let start = "Tokyo";
+  // let goal = "Osaka";
 
-  const scores = {
-    math: 80,
-    english: 90,
-  };
-  console.log(scores[`english`]);
-  scores.math = 40;
-  const entries = Object.entries(scores);
-  entries.forEach((entry) => {
-    console.log(`${entry[0]}: ${entry[1]}`);
-  });
-  // console.log(scores.math);
+  // [start, goal] = [goal, start];
+
+  /////////////////
+  const [first, ...others] = scores; //レスト構文
+  console.log(others);
+
+  const nums = [10, 20, 30];
+  const numBackup = [...nums];
 }
---
+++ javascript_learning/04/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+{
+  document.querySelector("button").addEventListener("click", () => {
+    // document.querySelector("p").textContent = "こんにちは";
+    document.querySelector("p").classList.add("pink-bg");
+  });
+}
