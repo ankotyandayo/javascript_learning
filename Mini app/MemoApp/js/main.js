38c088867b225e67dea4dbe20a8c1aef39cdfc94
安東秀斗 <shuto.ando1@gmail.com>
2024-04-29 09:50:02 +0800

    Iam god

---
+++ Mini app/MemoApp/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+{
+  const text = document.getElementById("text");
+  const save = document.getElementById("save");
+  const message = document.getElementById("message");
+
+  save.addEventListener("click", () => {
+    message.classList.add("appear");
+    setTimeout(() => {
+      message.classList.remove("appear");
+    }, 1000);
+    localStorage.setItem("memo", text.value);
+  });
+}
